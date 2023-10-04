908d9af7f52dd10da442409f5bc43812bb147bfc
LittleCanOpener <hrafn8@gmail.com>
2023-10-04 18:24:57 +0000

    Second assignment

--- dart/project02.dart
+++ dart/project02.dart
@@ -6,7 +6,6 @@ import 'dart:math';
 void main(){
   print('Please select a number');
   int num = int.parse(stdin.readLineSync()!);
-
   for (var i = 0; i <= 10; i++) {
       print("${num}*${i}=${num * i}");
     }
---
+++ dart/project03.dart
@@ -0,0 +1,42 @@
+import 'dart:core';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:math';
+
+void main(){
+  var list = [];
+  var sum = 0.0;
+  print('Select 5 Numbers!');
+  while(true){
+    int num = int.parse(stdin.readLineSync()!);
+    list.add(num);
+    // Feed Back!
+  /*
+    print('List Length ${list.length}');
+    print('$list');
+
+   */
+    if (list.length == 5 ){
+      for (var i = 0; i < list.length; i++) {
+        sum += list[i];
+        }
+      print(sum);
+      return;
+      }
+    }
+  }
+
+  /*
+  int sum = 0;
+
+  for (var i = 0; i < 10; i++){
+    print(sum++);
+    sum += i;
+  }
+  print('The correct $sum');
+  print('Please select a number');
+  for (var i = 0; i <= 10; i++) {
+    print("${num}*${i}=${num * i}");
+  }
+
+   */
