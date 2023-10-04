7981fb574c3674a9e26f3a7e9c890b4088b4cd31
LittleCanOpener <hrafn8@gmail.com>
2023-10-04 16:20:53 +0000

    Second assignment

---
+++ dart/project02.dart
@@ -0,0 +1,13 @@
+import 'dart:core';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:math';
+
+void main(){
+  print('Please select a number');
+  int num = int.parse(stdin.readLineSync()!);
+
+  for (var i = 0; i <= 10; i++) {
+      print("${num}*${i}=${num * i}");
+    }
+}
